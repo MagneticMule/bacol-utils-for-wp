3a8e37ed3b55e85ecd0b086438fc7e18e8c003ba
Thom Sweeney <skywriter@example.com>
2019-01-08 23:22:40 +0000

    Added REST support for playscript post_type.

--- includes/custom-post-manager.class.php
+++ includes/custom-post-manager.class.php
@@ -8,7 +8,8 @@
  */
 
 
-class PostManager {
+class PostManager
+{
 
     /**
      * The custom taxonomy for this post object. Within the context of
@@ -17,7 +18,7 @@ class PostManager {
      *
      * @link(https://codex.wordpress.org/Taxonomies)
      * @var array
-    **/
+     **/
     private $taxonomy;
 
     /**
@@ -55,12 +56,12 @@ class PostManager {
     public function __construct()
     {
         $this->taxonomy = array();
-        $this->labels   = array();
+        $this->labels = array();
     }
 
     /**
      * Rename the standard blog admin area from "Posts to " Group Blog".
-     * 
+     *
      */
     function changeBlogLabel()
     {
@@ -93,20 +94,20 @@ class PostManager {
 
     /**
      * Our taxonomy allows students and teachers to add specigic genres to each play script
-     * 
+     *
      */
     function buildPlayScriptTaxonomy()
     {
         $labels = array(
-            'name'              => 'Story Genres',
-            'singular_name'     => 'Story Genre',
-            'search_items'      => 'Search All Story Genres',
-            'all_items'         => 'All Story Genres',
-            'edit_item'         => 'Edit Story Genre',
-            'update_item'       => 'Update Story Genre',
-            'add_new_item'      => 'Add New Story Genre',
-            'new_item_name'     => 'New Story Genre',
-            'menu_name'         => 'Story Genres'
+            'name' => 'Story Genres',
+            'singular_name' => 'Story Genre',
+            'search_items' => 'Search All Story Genres',
+            'all_items' => 'All Story Genres',
+            'edit_item' => 'Edit Story Genre',
+            'update_item' => 'Update Story Genre',
+            'add_new_item' => 'Add New Story Genre',
+            'new_item_name' => 'New Story Genre',
+            'menu_name' => 'Story Genres'
         );
 
         register_taxonomy(
@@ -119,40 +120,40 @@ class PostManager {
                 'rewrite' => array(
                     'slug' => 'genres',
                     'with_front' => true
-                    ),
+                ),
                 'show_admin_column' => true
-                )
-            );
+            )
+        );
     }
 
-     /**
-      * Defines an array of labels to pass to the custom post type
-      *
-      * @see register_post_type()
-      * @link https://codex.wordpress.org/Function_Reference/register_post_type. 
-      */
+    /**
+     * Defines an array of labels to pass to the custom post type
+     *
+     * @see register_post_type()
+     * @link https://codex.wordpress.org/Function_Reference/register_post_type.
+     */
     function buildPlayScriptPost()
-     {
-         $labels = array(
-            'name' => __( 'Play Scripts' ),
-            'singular_name' => __( 'Play Script' ),
-            'add_new' => ( 'Add New Script' ),
-            'add_new_item' => ( 'Add New Play Script' ),
-            'edit_item' => ( 'Edit Script' ),
-            'new_item' => ( 'New Script' ),
-            'view_item' => ( 'View Play Script' ),
-            'not_found' => ( 'No Play Scripts found' ),
-            'not_found_in_trash' => ( 'No Play Scripts found in Trash' ),
-            'parent_item_colon' => ( 'Parent Play Script:' ),
-            'menu_name' => ( 'Play Scripts' ),
-            );
+    {
+        $labels = array(
+            'name' => __('Play Scripts'),
+            'singular_name' => __('Play Script'),
+            'add_new' => ('Add New Script'),
+            'add_new_item' => ('Add New Play Script'),
+            'edit_item' => ('Edit Script'),
+            'new_item' => ('New Script'),
+            'view_item' => ('View Play Script'),
+            'not_found' => ('No Play Scripts found'),
+            'not_found_in_trash' => ('No Play Scripts found in Trash'),
+            'parent_item_colon' => ('Parent Play Script:'),
+            'menu_name' => ('Play Scripts'),
+        );
 
-         $customPostArgs = array(
+        $customPostArgs = array(
             'labels' => $labels,
             'hierarchical' => true,
             'description' => 'Play Scripts',
-            'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', 'page-attributes' ),
-            'taxonomies' => array( 'genres' ),
+            'supports' => array('title', 'editor', 'author', 'thumbnail', 'revisions', 'page-attributes'),
+            'taxonomies' => array('genres'),
             'public' => true,
             'show_ui' => true,
             'show_in_menu' => true,
@@ -166,51 +167,53 @@ class PostManager {
             'can_export' => true,
             'rewrite' => true,
             'capability_type' => 'post',
-            'rewrite' => array( 'slug' => 'playscripts' )
-            );
+            'rewrite' => array('slug' => 'playscripts'),
+            'show_in_rest' => true
+        );
 
-         register_post_type( 'playscripts', $customPostArgs );
-     }
-
-     /**
-      * Creates a help tab for the Play Script post type.
-      * @see add_help_tab()
-      * @link https://codex.wordpress.org/Class_Reference/WP_Screen/add_help_tab
-      */
-      function addPlayScriptHelpTab() {
-        $screen = get_current_screen();
-        $screenIds = array( 'edit-playscripts', 'playscripts' );
-
-    if ( ! in_array( $screen->id, $screenIds ) ) {
-        return;
+        register_post_type('playscripts', $customPostArgs);
     }
 
-    $screen->add_help_tab(
-        array(
-            'id'      => 'sp_overview',
-            'title'   => 'Overview',
-            'content' => '<p>You can add the contents of a new PlayScript by either pasting in preformatted text from
+    /**
+     * Creates a help tab for the Play Script post type.
+     * @see add_help_tab()
+     * @link https://codex.wordpress.org/Class_Reference/WP_Screen/add_help_tab
+     */
+    function addPlayScriptHelpTab()
+    {
+        $screen = get_current_screen();
+        $screenIds = array('edit-playscripts', 'playscripts');
+
+        if (!in_array($screen->id, $screenIds)) {
+            return;
+        }
+
+        $screen->add_help_tab(
+            array(
+                'id' => 'sp_overview',
+                'title' => 'Overview',
+                'content' => '<p>You can add the contents of a new PlayScript by either pasting in preformatted text from
             a word processor or by using the built in word processor here. Either way, you can edit the text here using the tools
             provided.</p>'
-        )
-    );
+            )
+        );
 
-    $screen->add_help_tab(
-        array(
-            'id'      => 'sp_video',
-            'title'   => 'Video',
-            'content' => '<iframe width="720" height="405" src="https://www.youtube.com/embed/Avcp4m6BkLQ?rel=0" frameborder="1" allow="autoplay; encrypted-media" allowfullscreen style="padding:20px 0 0 20px;"></iframe>'
-        )
-    );
+        $screen->add_help_tab(
+            array(
+                'id' => 'sp_video',
+                'title' => 'Video',
+                'content' => '<iframe width="720" height="405" src="https://www.youtube.com/embed/Avcp4m6BkLQ?rel=0" frameborder="1" allow="autoplay; encrypted-media" allowfullscreen style="padding:20px 0 0 20px;"></iframe>'
+            )
+        );
 
 
-    $screen->add_help_tab(
-        array(
-            'id'      => 'sp_support',
-            'title'   => 'Support',
-            'content' => '<p>If you get really stuck, email Tommy at <a href="mailto:skywriter@gmail.com">skywriter@gmail.com</a></p>'
-        )
-    );
+        $screen->add_help_tab(
+            array(
+                'id' => 'sp_support',
+                'title' => 'Support',
+                'content' => '<p>If you get really stuck, email Tommy at <a href="mailto:skywriter@gmail.com">skywriter@gmail.com</a></p>'
+            )
+        );
 
 
     // Add a sidebar to contextual help.
@@ -219,30 +222,30 @@ class PostManager {
     }
 
     /**
-     * 
+     *
      * @param [type] $query [description]
      */
-    function addPostToQuery($query) {
-        if( is_home() && $query->is_main_query() )
-        {
-            $query->set( 'playscripts', 'post' );
+    function addPostToQuery($query)
+    {
+        if (is_home() && $query->is_main_query()) {
+            $query->set('playscripts', 'post');
         }
         return $query;
     }
 
     /**
      * The 'register_post_type' function needs to be called during the WP init phase of execution.
-     * note, when using this in a class based plugin we need to define the callback class 
+     * note, when using this in a class based plugin we need to define the callback class
      * "which is always $this" as the first element of an array.
      * @return null
      */
     public function registerPostManager()
-    {      
-        add_action( 'init', array($this, 'changeBlogLabel' ));
-        add_action( 'init', array($this, 'changeBlogObject' ));
-        add_action( 'init', array($this, 'buildPlayScriptPost' ));
-        add_action( 'init', array($this, 'buildPlayScriptTaxonomy' ));
-        add_action( "load-edit.php", array($this, 'addPlayScriptHelpTab' ));
-        add_action( "load-post.php", array($this, 'addPlayScriptHelpTab' ));
+    {
+        add_action('init', array($this, 'changeBlogLabel'));
+        add_action('init', array($this, 'changeBlogObject'));
+        add_action('init', array($this, 'buildPlayScriptPost'));
+        add_action('init', array($this, 'buildPlayScriptTaxonomy'));
+        add_action("load-edit.php", array($this, 'addPlayScriptHelpTab'));
+        add_action("load-post.php", array($this, 'addPlayScriptHelpTab'));
     }
 }
